857dd337f63074f88030b432b4c95115012d94ad
Snehal Kadlag <kadlagsnehal05@gmail.com>
2025-07-02 14:04:22 +0000

    DataTypes Learning

---
+++ 01-Basics/02_DataTypes.js
@@ -0,0 +1,17 @@
+"use strict";     // Treat all Js code as newer version
+let name ="Snehal"  //String Datatypes
+let age = 28        // number datatype
+let isloggedIN = false  //Boolean datatype 
+
+/* we have some more datatype like
+Null => Standalone Value
+Undefine => whenyou dont define any value
+Symbol => unique value
+Object => derived dataType
+*/
+
+console.log(typeof "abc")
+console.log(typeof null)  //object
+console.log(typeof age)
+console.log(typeof undefined)
+
