d001edaf9c4afd10167fd513e2b3e7ce83af1c3c
Snehal Kadlag <kadlagsnehal05@gmail.com>
2025-07-02 13:33:33 +0000

    Variables Learning

---
+++ 01-Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail ="Snehal@gmail.com"
+var accountPassword = "123456"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+let accountState;
+
+// if we define anything as constant we cannot change it later in the code
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/* Let and var is used for declaring constant prefered not to use var because of issuse
+in block scope and functional scope */
+
+// if u dont know the value which you want to give your variable we can leave it with just semi-colon it takes it undefine
+
