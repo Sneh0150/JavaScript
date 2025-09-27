ef690601c459baf11ae9bf4634375d93c10a545e
Snehal Kadlag <kadlagsnehal05@gmail.com>
2025-09-27 12:27:21 +0000

    separating data Conversion in different file.

--- 01-Basics/02_DataTypes.js
+++ 01-Basics/02_DataTypes.js
@@ -1,40 +1,25 @@
-"use strict";     // Treat all Js code as newer version
-let name ="Snehal"  //String Datatypes
-let age = 28        // number datatype
-let isloggedIN = false  //Boolean datatype 
+"use strict";                // Treat all Js code as newer version
+let name ="Snehal"          //String Datatypes
+let age = 28               // number datatype
+let isloggedIN = false    //Boolean datatype 
 
 /* we have some more datatype like
-Null => Standalone Value
-Undefine => whenyou dont define any value
-Symbol => unique value
-Object => derived dataType
+
+Null =>     Standalone Value. Which means Empty
+Undefine => whenyou dont define any value. Mean you have not define any
+            Value yet
+Symbol =>   unique value 
+Object =>   derived dataType
+
 */
 
-console.log(typeof "abc")
-console.log(typeof null)  //object
-console.log(typeof age)
-console.log(typeof undefined)
+// When you want to the datatype of any value we use typeof
 
-// conversion
+console.log(typeof "abc")                //string
+console.log(typeof null)                //object
+console.log(typeof age)                //number
+console.log(typeof undefined)         // Undefined
 
-let score ="33abc"
-console.log(typeof score)
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
 
-// Conversion on Boolean
-
-let Data = 1
-let NewData = Boolean(Data)
-console.log(NewData)
-
-let Data1 = ""
-let NewData1 = Boolean(Data1)
-console.log(NewData1)
-
-let Name1 = "Snehal"
-let NewName = Boolean(Name1)
-console.log(NewName)
 
 
