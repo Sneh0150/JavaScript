b86661d3f13cd5c581cb24de2a4a1f7bfa0d0350
Snehal Kadlag <kadlagsnehal05@gmail.com>
2025-10-08 10:57:34 +0000

    Add Prefix and PostFix

--- 01-Basics/04_Operations.js
+++ 01-Basics/04_Operations.js
@@ -10,3 +10,18 @@ let FirstName = "Snehal "
 let LastName = "Kadlag"
 let FullName = FirstName+LastName
 console.log(FullName) 
+
+//Case Prefix operation
+
+let GameCounter = 100
+let UpdatedScore = ++GameCounter
+console.log(GameCounter)                //101 value get Increase before Assigning
+console.log(UpdatedScore)              //101 updated value
+
+
+//Case Postfix operation
+
+let CounterValue = 100
+let NewScore = CounterValue++
+console.log(CounterValue)             //101 Counter value get increase after assigning
+console.log(NewScore)                //100 in Post case assignment is done first then value is increased
